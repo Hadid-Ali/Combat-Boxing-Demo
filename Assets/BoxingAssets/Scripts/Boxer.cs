6ae23ebc95e5212fb19db65e7f47977cf479dd2c
blunty.hadid <blunty.hadid@gmail.com>
2025-09-09 12:07:04 +0500

    updated

--- Assets/BoxingAssets/Scripts/Boxer.cs
+++ Assets/BoxingAssets/Scripts/Boxer.cs
@@ -5,82 +5,42 @@ public class Boxer : MonoBehaviour
     [SerializeField] Transform playerCameraTransform;
     [SerializeField] float delayToChooseCard;
 
-    public delegate void PowerUpAttack();
-    public static event PowerUpAttack onPowerup;
 
-    public delegate void PowerPunchAttack();
-    public static event PowerPunchAttack onPowerPunch;
+    #region Functions
 
-    public delegate void ComboAttack();
-    public static event ComboAttack onCombo;
-
-    public delegate void UpperCutAttack();
-    public static event UpperCutAttack onUppercut;
-
-    public delegate void OverHandAttack();
-    public static event OverHandAttack onOverhand;
-
-    public delegate void HookAttack();
-    public static event HookAttack onHook;
-
-    public delegate void BodyAttack();
-    public static event BodyAttack onBody;
-    public delegate void JabAttack();
-    public static event JabAttack onJab;
-    private void OnEnable()
-    {
-        onPowerup += PowerUp;
-        onPowerPunch += PowerPunch;
-        onCombo += Combo;
-        onUppercut += UpperCut;
-        onOverhand += OverHand;
-        onHook += Hook;
-        onBody += Body;
-        onJab += Jab;
-    }
-    private void OnDisable()
-    {
-        onPowerup -= PowerUp;
-        onPowerPunch -= PowerPunch;
-        onCombo -= Combo;
-        onUppercut -= UpperCut;
-        onOverhand -= OverHand;
-        onHook -= Hook;
-        onBody -= Body;
-        onJab -= Jab;
-    }
-    void PowerUp()
+    protected void PowerUp()
     {
 
     }
 
-    void PowerPunch()
+    protected void PowerPunch()
     {
 
     }
 
-    void Combo()
+    protected void Combo()
     {
 
     }
-    void UpperCut()
+    protected void UpperCut()
     {
 
     }
-    void OverHand()
+    protected void OverHand()
     {
 
     }
-    void Hook()
+    protected void Hook()
     {
 
     }
-    void Body()
+    protected void Body()
     {
 
     }
-    void Jab()
+    protected void Jab()
     {
 
     }
+    #endregion
 }
--- Assets/BoxingAssets/Scripts/Player.cs
+++ Assets/BoxingAssets/Scripts/Player.cs
@@ -1,7 +1,86 @@
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
 
 public class Player : Boxer
 {
-    
 
+    public delegate void PowerUpAttack();
+    public static event PowerUpAttack onPowerup;
+
+    public delegate void PowerPunchAttack();
+    public static event PowerPunchAttack onPowerPunch;
+
+    public delegate void ComboAttack();
+    public static event ComboAttack onCombo;
+
+    public delegate void UpperCutAttack();
+    public static event UpperCutAttack onUppercut;
+
+    public delegate void OverHandAttack();
+    public static event OverHandAttack onOverhand;
+
+    public delegate void HookAttack();
+    public static event HookAttack onHook;
+
+    public delegate void BodyAttack();
+    public static event BodyAttack onBody;
+
+    public delegate void JabAttack();
+    public static event JabAttack onJab;
+    private void OnEnable()
+    {
+        onPowerup += PowerUp;
+        onPowerPunch += PowerPunch;
+        onCombo += Combo;
+        onUppercut += UpperCut;
+        onOverhand += OverHand;
+        onHook += Hook;
+        onBody += Body;
+        onJab += Jab;
+    }
+    private void OnDisable()
+    {
+        onPowerup -= PowerUp;
+        onPowerPunch -= PowerPunch;
+        onCombo -= Combo;
+        onUppercut -= UpperCut;
+        onOverhand -= OverHand;
+        onHook -= Hook;
+        onBody -= Body;
+        onJab -= Jab;
+    }
+    #region Events Invoke
+    public static void OnPowerUpAttack()
+    {
+        onPowerup?.Invoke();
+    }
+    public static void OnPowerPunchAttack()
+    {
+        onPowerPunch?.Invoke();
+    }
+    public static void OnComboAttack()
+    {
+        onCombo?.Invoke();
+    }
+    public static void OnUppercutAttack()
+    {
+        onUppercut?.Invoke();
+    }
+    public static void OnOverhandAttack()
+    {
+        onOverhand?.Invoke();
+    }
+    public static void OnHookAttack()
+    {
+        onHook?.Invoke();
+    }
+    public static void OnBodyAttack()
+    {
+        onBody?.Invoke();
+    }
+    public static void OnJabAttack()
+    {
+        onJab?.Invoke();
+    }
+    #endregion
 }
